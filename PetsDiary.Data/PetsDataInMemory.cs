8e0141fec976bf5f50f7862e79d4c24aca411e61
Asthenia182 <agembarzewska@gmail.com>
2021-02-17 12:44:00 +0100

    fixed error in petsdatainmemory

--- PetsDiary.Data/PetsDataInMemory.cs
+++ PetsDiary.Data/PetsDataInMemory.cs
@@ -8,11 +8,11 @@ namespace PetsDiary.Data
 {
     public class PetsDataInMemory : IPetsData
     {
+        private readonly List<NoteModel> notes;
         private readonly List<PetModel> pets;
         private readonly List<VaccinationModel> vaccinations;
         private readonly List<VisitModel> visits;
         private readonly List<WeightModel> weights;
-        private readonly List<NoteModel> notes;
 
         public PetsDataInMemory()
         {
@@ -53,16 +53,40 @@ namespace PetsDiary.Data
             };
         }
 
-        public IEnumerable<NoteModel> GetNotes(int petId)
-        {
-            return notes
-                .Where(x => x.PetId == petId);
-        }
-
         public NoteModel AddNote(NoteModel model)
         {
             notes.Add(model);
             model.Id = notes.Max(r => r.Id) + 1;
+            return model;
+        }
+
+        public PetModel AddPet(PetModel model)
+        {
+            model.Id = pets.Max(r => r.Id) + 1;
+            model.LastModified = DateTime.Now;
+            pets.Add(model);
+
+            return model;
+        }
+
+        public VaccinationModel AddVacination(VaccinationModel model)
+        {
+            vaccinations.Add(model);
+            model.Id = vaccinations.Max(r => r.Id) + 1;
+            return model;
+        }
+
+        public VisitModel AddVisit(VisitModel model)
+        {
+            visits.Add(model);
+            model.Id = visits.Max(r => r.Id) + 1;
+            return model;
+        }
+
+        public WeightModel AddWeight(WeightModel model)
+        {
+            weights.Add(model);
+            model.Id = weights.Max(r => r.Id) + 1;
             return model;
         }
 
@@ -75,137 +99,13 @@ namespace PetsDiary.Data
             }
         }
 
-        public NoteModel UpdateNote(NoteModel updatedWeight)
+        public void DeleteNoteByPetId(int petId)
         {
-            var note = notes.SingleOrDefault(r => r.Id == updatedWeight.Id);
-
+            var note = this.notes.FirstOrDefault(r => r.PetId == petId);
             if (note != null)
             {
-                note.Id = updatedWeight.Id;
-                note.PetId = updatedWeight.PetId;
-                note.Note = updatedWeight.Note;
+                notes.Remove(note);
             }
-
-            return note;
-        }
-
-        public IEnumerable<WeightModel> GetWeights(int petId)
-        {
-            return weights
-                .Where(x => x.PetId == petId)
-                .OrderByDescending(x => x.Date);
-        }
-
-        public WeightModel AddWeight(WeightModel model)
-        {
-            weights.Add(model);
-            model.Id = weights.Max(r => r.Id) + 1;
-            return model;
-        }
-
-        public void DeleteWeightById(int id)
-        {
-            var weight = weights.FirstOrDefault(r => r.Id == id);
-            if (weight != null)
-            {
-                weights.Remove(weight);
-            }
-        }
-
-        public WeightModel UpdateWeight(WeightModel updatedWeight)
-        {
-            var weight = weights.SingleOrDefault(r => r.Id == updatedWeight.Id);
-
-            if (weight != null)
-            {
-                weight.Id = updatedWeight.Id;
-                weight.PetId = updatedWeight.PetId;
-                weight.Weight = updatedWeight.Weight;
-                weight.Date = updatedWeight.Date;
-            }
-
-            return weight;
-        }
-
-        public IEnumerable<VisitModel> GetVisits(int petId)
-        {
-            return visits
-                .Where(x => x.PetId == petId)
-                .OrderByDescending(x => x.Date);
-        }
-
-        public VisitModel AddVisit(VisitModel model)
-        {
-            visits.Add(model);
-            model.Id = visits.Max(r => r.Id) + 1;
-            return model;
-        }
-
-        public void DeleteVisitById(int id)
-        {
-            var visit = visits.FirstOrDefault(r => r.Id == id);
-            if (visit != null)
-            {
-                visits.Remove(visit);
-            }
-        }
-
-        public VisitModel UpdateVisit(VisitModel updatedVisit)
-        {
-            var visit = visits.SingleOrDefault(r => r.Id == updatedVisit.Id);
-
-            if (visit != null)
-            {
-                visit.Id = updatedVisit.Id;
-                visit.PetId = updatedVisit.PetId;
-                visit.Description = updatedVisit.Description;
-                visit.Date = updatedVisit.Date;
-            }
-
-            return visit;
-        }
-
-        public IEnumerable<VaccinationModel> GetVaccinations(int petId)
-        {
-            return vaccinations
-                .Where(x => x.PetId == petId)
-                .OrderBy(x => x.ShotDate);
-        }
-
-        public IEnumerable<PetModel> GetPets()
-        {
-            return pets.OrderBy(x => x.Name);
-        }
-
-        public PetModel GetPetById(int id)
-        {
-            return pets.SingleOrDefault(r => r.Id == id);
-        }
-
-        public PetModel AddPet(PetModel model)
-        {
-            model.Id = pets.Max(r => r.Id) + 1;
-            model.LastModified = DateTime.Now;
-            pets.Add(model);
-
-            return model;
-        }
-
-        public PetModel UpdatePet(PetModel updatedmodel)
-        {
-            var pet = pets.SingleOrDefault(r => r.Id == updatedmodel.Id);
-            if (pet != null)
-            {
-                pet.Id = updatedmodel.Id;
-                pet.Name = updatedmodel.Name;
-                pet.Breed = updatedmodel.Breed;
-                pet.AnimalType = updatedmodel.AnimalType;
-                pet.LastModified = DateTime.Now;
-                pet.BirthDate = updatedmodel.BirthDate;
-                pet.Gender = updatedmodel.Gender;
-                pet.Image = updatedmodel.Image;
-            }
-            return pet;
         }
 
         public void DeletePetById(int id)
@@ -220,13 +120,6 @@ namespace PetsDiary.Data
                 DeleteVaccinationByPetId(id);
                 DeleteWeightByPetId(id);
             }
-        }
-
-        public VaccinationModel AddVacination(VaccinationModel model)
-        {
-            vaccinations.Add(model);
-            model.Id = vaccinations.Max(r => r.Id) + 1;
-            return model;
         }
 
         public void DeleteVaccinationById(int id)
@@ -247,6 +140,15 @@ namespace PetsDiary.Data
             }
         }
 
+        public void DeleteVisitById(int id)
+        {
+            var visit = visits.FirstOrDefault(r => r.Id == id);
+            if (visit != null)
+            {
+                visits.Remove(visit);
+            }
+        }
+
         public void DeleteVisitByPetId(int petId)
         {
             var visit = this.visits.FirstOrDefault(r => r.PetId == petId);
@@ -256,12 +158,12 @@ namespace PetsDiary.Data
             }
         }
 
-        public void DeleteNoteByPetId(int petId)
+        public void DeleteWeightById(int id)
         {
-            var note = this.notes.FirstOrDefault(r => r.PetId == petId);
-            if (note != null)
+            var weight = weights.FirstOrDefault(r => r.Id == id);
+            if (weight != null)
             {
-                notes.Remove(note);
+                weights.Remove(weight);
             }
         }
 
@@ -270,8 +172,76 @@ namespace PetsDiary.Data
             var weight = this.weights.FirstOrDefault(r => r.PetId == petId);
             if (weight != null)
             {
-                notes.Remove(weight);
+                weights.Remove(weight);
             }
+        }
+
+        public IEnumerable<NoteModel> GetNotes(int petId)
+        {
+            return notes
+                .Where(x => x.PetId == petId);
+        }
+
+        public PetModel GetPetById(int id)
+        {
+            return pets.SingleOrDefault(r => r.Id == id);
+        }
+
+        public IEnumerable<PetModel> GetPets()
+        {
+            return pets.OrderBy(x => x.Name);
+        }
+
+        public IEnumerable<VaccinationModel> GetVaccinations(int petId)
+        {
+            return vaccinations
+                .Where(x => x.PetId == petId)
+                .OrderBy(x => x.ShotDate);
+        }
+
+        public IEnumerable<VisitModel> GetVisits(int petId)
+        {
+            return visits
+                .Where(x => x.PetId == petId)
+                .OrderByDescending(x => x.Date);
+        }
+
+        public IEnumerable<WeightModel> GetWeights(int petId)
+        {
+            return weights
+                .Where(x => x.PetId == petId)
+                .OrderByDescending(x => x.Date);
+        }
+
+        public NoteModel UpdateNote(NoteModel updatedWeight)
+        {
+            var note = notes.SingleOrDefault(r => r.Id == updatedWeight.Id);
+
+            if (note != null)
+            {
+                note.Id = updatedWeight.Id;
+                note.PetId = updatedWeight.PetId;
+                note.Note = updatedWeight.Note;
+            }
+
+            return note;
+        }
+
+        public PetModel UpdatePet(PetModel updatedmodel)
+        {
+            var pet = pets.SingleOrDefault(r => r.Id == updatedmodel.Id);
+            if (pet != null)
+            {
+                pet.Id = updatedmodel.Id;
+                pet.Name = updatedmodel.Name;
+                pet.Breed = updatedmodel.Breed;
+                pet.AnimalType = updatedmodel.AnimalType;
+                pet.LastModified = DateTime.Now;
+                pet.BirthDate = updatedmodel.BirthDate;
+                pet.Gender = updatedmodel.Gender;
+                pet.Image = updatedmodel.Image;
+            }
+            return pet;
         }
 
         public VaccinationModel UpdateVaccination(VaccinationModel updatedVaccination)
@@ -287,6 +257,36 @@ namespace PetsDiary.Data
                 vaccination.Address = updatedVaccination.Address;
             }
             return vaccination;
+        }
+
+        public VisitModel UpdateVisit(VisitModel updatedVisit)
+        {
+            var visit = visits.SingleOrDefault(r => r.Id == updatedVisit.Id);
+
+            if (visit != null)
+            {
+                visit.Id = updatedVisit.Id;
+                visit.PetId = updatedVisit.PetId;
+                visit.Description = updatedVisit.Description;
+                visit.Date = updatedVisit.Date;
+            }
+
+            return visit;
+        }
+
+        public WeightModel UpdateWeight(WeightModel updatedWeight)
+        {
+            var weight = weights.SingleOrDefault(r => r.Id == updatedWeight.Id);
+
+            if (weight != null)
+            {
+                weight.Id = updatedWeight.Id;
+                weight.PetId = updatedWeight.PetId;
+                weight.Weight = updatedWeight.Weight;
+                weight.Date = updatedWeight.Date;
+            }
+
+            return weight;
         }
     }
 }
